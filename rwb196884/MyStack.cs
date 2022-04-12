61ed8b08e06c3388a59b559ecd52a369c6a6447a
richardb <richardb@truthsoftware.co.uk>
2022-04-12 10:49:42 +0100

    Add namespace.

--- rwb196884/MyStack.cs
+++ rwb196884/MyStack.cs
@@ -2,44 +2,47 @@ using Pulumi;
 using System;
 using System.Diagnostics;
 
-class MyStack : Stack
+namespace rwb196884
 {
-    public MyStack()
+    class MyStack : Stack
     {
-        Console.WriteLine("Hello World from Console.WriteLine."); // Outputs on `pulumi up` and into the log at https://app.pulumi.com/.../rwb196884/dev/updates/1.
-        Debug.WriteLine("Hello World from Console.WriteLine."); // Doesn't do anything.
-        Pulumi.Log.Info("Hello World from Pulumi.Log.Info."); // Outputs on `pulumi up` and into the log at https://app.pulumi.com/.../rwb196884/dev/updates/1
+        public MyStack()
+        {
+            Console.WriteLine("Hello World from Console.WriteLine."); // Outputs on `pulumi up` and into the log at https://app.pulumi.com/.../rwb196884/dev/updates/1.
+            Debug.WriteLine("Hello World from Console.WriteLine."); // Doesn't do anything.
+            Pulumi.Log.Info("Hello World from Pulumi.Log.Info."); // Outputs on `pulumi up` and into the log at https://app.pulumi.com/.../rwb196884/dev/updates/1
 
 
-        //// Create an Azure Resource Group
-        //var resourceGroup = new ResourceGroup("resourceGroup");
+            //// Create an Azure Resource Group
+            //var resourceGroup = new ResourceGroup("resourceGroup");
 
-        //// Create an Azure resource (Storage Account)
-        //var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+            //// Create an Azure resource (Storage Account)
+            //var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+            //{
+            //    ResourceGroupName = resourceGroup.Name,
+            //    Sku = new SkuArgs
+            //    {
+            //        Name = SkuName.Standard_LRS
+            //    },
+            //    Kind = Kind.StorageV2
+            //});
+
+            //// Export the primary key of the Storage Account
+            //this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
+            //    Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
+        }
+
+        //[Output]
+        //public Output<string> PrimaryStorageKey { get; set; }
+
+        //private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
         //{
-        //    ResourceGroupName = resourceGroup.Name,
-        //    Sku = new SkuArgs
+        //    var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
         //    {
-        //        Name = SkuName.Standard_LRS
-        //    },
-        //    Kind = Kind.StorageV2
-        //});
-
-        //// Export the primary key of the Storage Account
-        //this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
-        //    Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
+        //        ResourceGroupName = resourceGroupName,
+        //        AccountName = accountName
+        //    });
+        //    return accountKeys.Keys[0].Value;
+        //}
     }
-
-    //[Output]
-    //public Output<string> PrimaryStorageKey { get; set; }
-
-    //private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
-    //{
-    //    var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
-    //    {
-    //        ResourceGroupName = resourceGroupName,
-    //        AccountName = accountName
-    //    });
-    //    return accountKeys.Keys[0].Value;
-    //}
 }
